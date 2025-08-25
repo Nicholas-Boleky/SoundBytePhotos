8c0b0656fb67a5d47206d75470ad016b8c57c024
Nicholas Boleky <bolekyn@gmail.com>
2025-08-25 12:09:00 -0700

    First Commit project initialization

---
+++ SoundBytePhotos/SoundBytePhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundBytePhotosApp.swift
+//  SoundBytePhotos
+//
+//  Created by Nicholas Boleky on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SoundBytePhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
